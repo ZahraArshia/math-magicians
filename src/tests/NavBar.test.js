9ce32f20afdeb32c35d0b42fe6d0d2f62f48c3ba
Ackon Samuel <ackons433@gmail.com>
2022-04-01 14:15:59 +0000

    Update NavBar.test.js

--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
-it('NavBar renders correctly', () => {
+test('NavBar renders accurately', () => {
   render(
     <BrowserRouter>
       <NavBar />
