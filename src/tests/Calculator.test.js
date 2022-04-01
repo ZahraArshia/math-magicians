2b0cebc1ca74649cedfe88f47da4e4ed487e7808
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-04-01 22:53:11 +0430

    user interaction tests added

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Calculator from '../components/Calculator';
 
 it('Calculator renders correctly', () => {
   const tree = render(<Calculator />);
   expect(tree).toMatchSnapshot();
+});
+
+it('Calculator user interaction', () => {
+  render(
+    <BrowserRouter>
+      <Calculator />
+    </BrowserRouter>,
+  );
+  const Element = screen.getByText('Lets do some math!');
+  expect(Element).toBeInTheDocument();
 });
--- src/tests/Quote.test.js
+++ src/tests/Quote.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Quote from '../components/Quote';
 
 it('Quote renders correctly', () => {
   const tree = render(<Quote />);
   expect(tree).toMatchSnapshot();
+});
+
+it('Quote user interaction', () => {
+  render(
+    <BrowserRouter>
+      <Quote />
+    </BrowserRouter>,
+  );
+  const Element = screen.getByText('- William Paul Thurston');
+  expect(Element).toBeInTheDocument();
 });
